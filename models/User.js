47643208b6540f29c6f1afd5a8d35e39ca27b8f1
Raimundo Luiz <rayfcrols@gmail.com>
2019-08-27 17:17:09 -0300

    Config Prettie and eslint

---
+++ models/User.js
@@ -0,0 +1,12 @@
+class User {
+  constructor(name, gender, birth, email, country, password, photo, admin) {
+    this.name = name;
+    this.gender = gender;
+    this.birth = birth;
+    this.email = email;
+    this.country = country;
+    this.password = password;
+    this.photo = photo;
+    this.admin = admin;
+  }
+}
